b9eff6175ef89aee2cc0c38880c286fb33192a6d
SK-Mantle <aidan.imenard@gmail.com>
2021-02-23 18:48:40 -0600

    Midterm File Push

---
+++ Midterm/wheelSpokes.js
@@ -0,0 +1,92 @@
+var SolarSystem = {
+  middleWheel : {
+    radius : 3,  
+    distance : 0,
+    year : 0,
+    color : [ 1.0, 1.0, 0.0, 1.0 ]
+  },
+  spokeOneOne : {
+    radius : 2,
+    distance : 11,
+    year : 2,
+	axis : [0.0, 0.0, 1.0],
+    color : [ 0.5, 0.5, 0.5, 1.0 ]
+  },
+  spokeOneTwo : {
+    radius : 2,
+    distance : 8,
+    year : 2,
+	axis : [0.0, 0.0, 1.0],
+    color : [ 0.5, 0.5, 0.5, 1.0 ]
+  },
+  spokeOneThree : {
+    radius : 2,
+    distance : 4.5,
+    year : 2,
+	axis : [0.0, 0.0, 1.0],
+    color : [ 0.5, 0.5, 0.5, 1.0 ]
+  },
+  spokeTwoOne : {
+    radius : 2,
+    distance : 11,
+    year : 2,
+	axis : [0.0, 0.0, 1.0],
+    color : [ 0.5, 0.5, 0.5, 1.0 ]
+  },
+  spokeTwoTwo : {
+    radius : 2,
+    distance : 8,
+    year : 2,
+	axis : [0.0, 0.0, 1.0],
+    color : [ 0.5, 0.5, 0.5, 1.0 ]
+  },
+  spokeTwoThree : {
+    radius : 2,
+    distance : 4.5,
+    year : 2,
+	axis : [0.0, 0.0, 1.0],
+    color : [ 0.5, 0.5, 0.5, 1.0 ]
+  },
+  spokeThreeOne : {
+    radius : 2,
+    distance : 8,
+    year : 2,
+	axis : [0.0, 0.0, 1.0],
+    color : [ 0.5, 0.5, 0.5, 1.0 ]
+  },
+  spokeThreeTwo : {
+    radius : 2,
+    distance : 4.5,
+    year : 2,
+	axis : [0.0, 0.0, 1.0],
+    color : [ 0.5, 0.5, 0.5, 1.0 ]
+  },
+    spokeThreeThree : {
+    radius : 2,
+    distance : 11,
+    year : 2,
+	axis : [0.0, 0.0, 1.0],
+    color : [ 0.5, 0.5, 0.5, 1.0 ]
+  },
+  spokeFourOne : {
+	radius : 2,
+    distance : 8,
+    year : 2,
+	axis : [0.0, 0.0, 1.0],
+    color : [ 0.5, 0.5, 0.5, 1.0 ]
+  },
+  spokeFourTwo : {
+	radius : 2,
+    distance : 4,
+    year : 2,
+	axis : [0.0, 0.0, 1.0],
+    color : [ 0.5, 0.5, 0.5, 1.0 ]
+  },
+    spokeFourThree : {
+    radius : 2,
+    distance : 11,
+    year : 2,
+	axis : [0.0, 0.0, 1.0],
+    color : [ 0.5, 0.5, 0.5, 1.0 ]
+  }
+};
